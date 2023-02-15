99b0b6d0027cad4c3d1fb491ea454946566a0812
Shakhbozbek Usmonov <83509440+shakhbozbekusmonov@users.noreply.github.com>
2023-02-15 11:44:26 +0500

    Filtered and Sorted movies done

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ const elMoviesTemplate = document.querySelector(".js-movies-temp").content;
 const newMoviesFragment = new DocumentFragment();
 // ----------------------------------------------------------------
 const elSearchForm = document.querySelector(".js-search-form");
-const elSearchFormInput = elSearchForm.querySelector(".js-search-form-input");
-const elSearchFormSelect = elSearchForm.querySelector(".js-search-form-select");
-const elSearchFormSort = elSearchForm.querySelector(".js-search-form-sort");
+const elSearchInput = elSearchForm.querySelector(".js-search-input");
+const elGenresSelect = elSearchForm.querySelector(".js-select-genres");
+const elFromYear = elSearchForm.querySelector(".js-from-year");
+const elToYear = elSearchForm.querySelector(".js-to-year");
+const elSortMovies = elSearchForm.querySelector(".js-movies-sort");
 
 // NORMALIZE MOVIES
 const normalizeMovies = movies.map((movie) => {
@@ -26,29 +28,30 @@ const normalizeMovies = movies.map((movie) => {
 });
 
 // GENERATE CATEGORIES
-let selectFragment = new DocumentFragment();
-const categories = [];
+let genresFragment = new DocumentFragment();
+const genres = [];
 function generateCategories(movies) {
   movies.forEach((movie) => {
-    movie.movie_categories.split("|").forEach((category) => {
-      if (!categories.includes(category)) {
-        categories.push(category);
+    movie.movie_categories.split("|").forEach((genre) => {
+      if (!genres.includes(genre)) {
+        genres.push(genre);
       }
     });
   });
+  genres.sort();
 }
 
 generateCategories(normalizeMovies);
 
-categories.forEach((genre) => {
+genres.forEach((genre) => {
   let genreOption = document.createElement("option");
   genreOption.textContent = genre;
   genreOption.value = genre;
-  
-  selectFragment.append(genreOption);
+
+  genresFragment.append(genreOption);
 });
 
-elSearchFormSelect.appendChild(selectFragment);
+elGenresSelect.appendChild(genresFragment);
 
 
 // GET MODAL ELEMENTS FROM DOM
@@ -69,38 +72,37 @@ function getDuration(time) {
   return `${hours} hrs ${minuts} min`;
 }
 
-// RENDER FUNCTION 
+// RENDER FUNCTION
 function renderMovies(movies) {
-  
   elMoviesList.innerHTML = null;
-  
+
   movies.forEach((movie) => {
     const elCloneMovie = elMoviesTemplate.cloneNode(true);
-    
+
     elCloneMovie.querySelector(
       ".js-movies-img"
-      ).src = `http://i3.ytimg.com/vi/${movie.movie_ytid}/hqdefault.jpg`;
-      elCloneMovie.querySelector(".js-movies-img").alt = movie.movie_title;
-      elCloneMovie.querySelector(".js-movies-title").textContent =
+    ).src = `http://i3.ytimg.com/vi/${movie.movie_ytid}/hqdefault.jpg`;
+    elCloneMovie.querySelector(".js-movies-img").alt = movie.movie_title;
+    elCloneMovie.querySelector(".js-movies-title").textContent =
       movie.movie_title;
-      elCloneMovie.querySelector(".js-movies-rating").textContent =
+    elCloneMovie.querySelector(".js-movies-rating").textContent =
       movie.movie_imdb_rating;
-      elCloneMovie.querySelector(".js-movies-year").textContent =
+    elCloneMovie.querySelector(".js-movies-year").textContent =
       movie.movie_year;
-      elCloneMovie.querySelector(".js-movies-runtime").textContent = getDuration(
-        movie.movie_runtime
-        );
-      elCloneMovie.querySelector(".js-movies-categories").textContent =
+    elCloneMovie.querySelector(".js-movies-runtime").textContent = getDuration(
+      movie.movie_runtime
+    );
+    elCloneMovie.querySelector(".js-movies-categories").textContent =
       movie.movie_categories.split("|").join(", ");
-        elCloneMovie.querySelector(".js-movies-btn").dataset.id =
+    elCloneMovie.querySelector(".js-movies-btn").dataset.id =
       movie.movie_imdb_id;
-        
-      newMoviesFragment.appendChild(elCloneMovie);
-    });
-      
+
+    newMoviesFragment.appendChild(elCloneMovie);
+  });
+
   elMoviesList.appendChild(newMoviesFragment);
 }
-    
+
 // RENDER MODAL INFO
 function renderModalInfo(findMovie) {
   elModalTitle.textContent = findMovie.movie_title;
@@ -114,15 +116,15 @@ function renderModalInfo(findMovie) {
   elModalSummary.textContent = findMovie.movie_summary;
   elModalLink.href = `https://www.imdb.com/title/${findMovie.movie_imdb_id}`;
 }
-    
+
 // MODAL RENDER EVENT DELEGATION
 elMoviesList.addEventListener("click", function (evt) {
   const elementTarget = evt.target;
   const btnId = elementTarget.dataset.id;
-      
+
   if (elementTarget.matches(".js-movies-btn")) {
-     const foundMovie = normalizeMovies.find((movie) => movie.imdb_id === btnId);
-     renderModalInfo(foundMovie);
+    const foundMovie = normalizeMovies.find((movie) => movie.movie_imdb_id === btnId);
+    renderModalInfo(foundMovie);
   }
 });
 
@@ -130,24 +132,92 @@ elMoviesList.addEventListener("click", function (evt) {
 elModal.addEventListener("hide.bs.modal", function () {
   elModalIframe.src = null;
 });
-    
+
 // SEARCH MOVIES
+function showSearchMovies(search){
+  const filteredMovies = normalizeMovies.filter(
+    (movie) => {
+     const moreCriteries = String(movie.movie_title).match(search) &&
+      (elGenresSelect.value == "all" ||
+        movie.movie_categories.includes(elGenresSelect.value)) &&
+      (elFromYear.value == "" ||
+        (movie.movie_year >= Number(elFromYear.value) &&
+          (elToYear.value == "" || movie.movie_year <= Number(elToYear.value))));
+      return moreCriteries;
+    });
+  return filteredMovies;
+}
+
+// SORTED MOVIES
+function sortMovies(movies, sortType){
+  if(sortType == "a-z"){
+    movies.sort((a, b) => {
+      if (String(a.movie_title).toLowerCase() > String(b.movie_title).toLowerCase())
+        return 1;
+      else if (
+        String(a.movie_title).toLowerCase() <
+        String(b.movie_title).toLowerCase()
+      )
+        return -1;
+      else return 0;
+    })
+  }
+
+  if(sortType == "z-a"){
+    movies.sort((a, b) => {
+      if (
+        String(a.movie_title).toLowerCase() >
+        String(b.movie_title).toLowerCase()
+      )
+        return -1;
+      else if (
+        String(a.movie_title).toLowerCase() <
+        String(b.movie_title).toLowerCase()
+      )
+        return 1;
+      else return 0;
+    });
+  }
+
+  if (sortType == "oldest-latest") {
+    movies.sort((a, b) => a.movie_year - b.movie_year); 
+  }
+
+  if (sortType == "latest-oldest") {
+    movies.sort((a, b) => b.movie_year - a.movie_year);
+  }
+
+  if (sortType == "low-rated") {
+    movies.sort((a, b) => a.movie_imdb_rating - b.movie_imdb_rating);
+  }
+
+  if (sortType == "high-rated") {
+    movies.sort((a, b) => b.movie_imdb_rating - a.movie_imdb_rating);
+  }
+}
+
+// SEARCH EVENTS  
 elSearchForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
 
-  const searchQuery = new RegExp(elSearchFormInput.value, "gi");
-  const foundMovies = [];
-
-  foundMovies = normalizeMovies.filter((movie) =>
-    String(movie.movie_title).match(searchQuery)
-  );
-
-  if (foundMovies.length > 0) {
-    renderMovies(foundMovies);
+  const searchQuery = new RegExp(elSearchInput.value, "gi");
+  const searchMovies = showSearchMovies(searchQuery);
+  if(searchMovies.length > 0){
+    sortMovies(searchMovies, elSortMovies.value);
+    renderMovies(searchMovies);
   } else {
-    elMoviesList.innerHTML = `<div class='text-white display-3'>"${elSearchFormInput.value}" movie  Not found🙄</div>`;
+    elMoviesList.innerHTML = `<div class='d-flex flex-column'>
+      <p class="text-white display-3">"${elSearchInput.value}" movie  Not found🙄</p>
+      <a class="text-white w-25 back-link text-decoration-underline">Back</a>
+    </div>`;
   }
 });
-    
+// BACK LOCATION EVENTS
+elMoviesList.addEventListener("click", (evt) => {
+  if(evt.target.matches(".back-link")){
+    window.location.reload();
+  }
+});
+
+// RENDER MOVIES
 renderMovies(normalizeMovies.slice(0, 10));
-    
